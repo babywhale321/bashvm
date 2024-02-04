a97208be19195498cb588bcc176f9f406b4ec0ec
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2024-02-03 21:45:35 -0500

    Update vps-manager-installer.sh

--- vps-manager-installer.sh
+++ vps-manager-installer.sh
@@ -4,11 +4,12 @@
 #
 #Author: Kyle Schroeder "BabyWhale"
 
-apt install qemu-kvm || echo "Installing qemu-kvm has failed. Please check the logs or console output above."
-apt install libvirt-daemon-system || echo "Installing libvirt-daemon-system has failed. Please check the logs or console output above."
-apt install libvirt-clients || echo "Installing libvirt-clients has failed. Please check the logs or console output above."
-apt install bridge-utils || echo "Installing bridge-utils has failed. Please check the logs or console output above."
-apt install git || echo "Installing git has failed. Please check the logs or console output above."
+apt install qemu-kvm -y || echo "Installing qemu-kvm has failed. Please check the logs or console output above."
+apt install libvirt-daemon-system -y || echo "Installing libvirt-daemon-system has failed. Please check the logs or console output above."
+apt install libvirt-clients -y || echo "Installing libvirt-clients has failed. Please check the logs or console output above."
+apt install bridge-utils -y || echo "Installing bridge-utils has failed. Please check the logs or console output above."
+apt install git -y || echo "Installing git has failed. Please check the logs or console output above."
+apt install neofetch -y || echo "Installing neofetch has failed. Please check the logs or console output above."
 
 git clone https://github.com/novnc/noVNC.git || echo "cloning novnc has failed. Please check the logs or console output above."
 
