c868a618811c8c5da0dad3fedba97fa2bf625f3c
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2024-02-06 23:17:06 -0500

    Update bashvm-installer.sh

--- bashvm-installer.sh
+++ bashvm-installer.sh
@@ -8,11 +8,14 @@ apt install qemu-kvm -y || echo "Installing qemu-kvm has failed. Please check th
 apt install libvirt-daemon-system -y || echo "Installing libvirt-daemon-system has failed. Please check the logs or console output above."
 apt install libvirt-clients -y || echo "Installing libvirt-clients has failed. Please check the logs or console output above."
 apt install bridge-utils -y || echo "Installing bridge-utils has failed. Please check the logs or console output above."
+apt install dnsmasq -y || echo "Installing dnsmasq has failed. Please check the logs or console output above."
+apt install qemu-utils -y || echo "Installing qemu-utils has failed. Please check the logs or console output above."
 apt install git -y || echo "Installing git has failed. Please check the logs or console output above."
 apt install htop -y || echo "Installing htop has failed. Please check the logs or console output above."z
 apt install net-tools -y || echo "Installing net-tools has failed. Please check the logs or console output above."
 
 git clone https://github.com/novnc/noVNC.git || echo "cloning novnc has failed. Please check the logs or console output above."
 
+echo ""
 echo "If no errors then everything should be up and ready to use!"
 echo "Run vps-manager.sh to use the interface."
