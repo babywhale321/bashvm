9752219f5de58fc87c6c3f04e0485d14865cf3cf
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2024-02-07 17:14:18 -0500

    Update bashvm-installer.sh

--- bashvm-installer.sh
+++ bashvm-installer.sh
@@ -10,11 +10,8 @@ apt install libvirt-clients -y || echo "Installing libvirt-clients has failed. P
 apt install bridge-utils -y || echo "Installing bridge-utils has failed. Please check the logs or console output above."
 apt install dnsmasq -y || echo "Installing dnsmasq has failed. Please check the logs or console output above."
 apt install qemu-utils -y || echo "Installing qemu-utils has failed. Please check the logs or console output above."
-apt install git -y || echo "Installing git has failed. Please check the logs or console output above."
 apt install htop -y || echo "Installing htop has failed. Please check the logs or console output above."z
 apt install net-tools -y || echo "Installing net-tools has failed. Please check the logs or console output above."
-
-git clone https://github.com/novnc/noVNC.git || echo "cloning novnc has failed. Please check the logs or console output above."
 
 echo ""
 echo "If no errors then everything should be up and ready to use!"
