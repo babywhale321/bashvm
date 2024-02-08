d9c2bd16da3cfdd992bc1957e39d3fd3db730486
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2024-02-08 02:04:07 -0500

    Update bashvm-installer.sh

--- bashvm-installer.sh
+++ bashvm-installer.sh
@@ -8,9 +8,7 @@ apt install qemu-kvm -y || echo "Installing qemu-kvm has failed. Please check th
 apt install libvirt-daemon-system -y || echo "Installing libvirt-daemon-system has failed. Please check the logs or console output above."
 apt install libvirt-clients -y || echo "Installing libvirt-clients has failed. Please check the logs or console output above."
 apt install bridge-utils -y || echo "Installing bridge-utils has failed. Please check the logs or console output above."
-apt install dnsmasq -y || echo "Installing dnsmasq has failed. Please check the logs or console output above."
-apt install qemu-utils -y || echo "Installing qemu-utils has failed. Please check the logs or console output above."
-apt install htop -y || echo "Installing htop has failed. Please check the logs or console output above."z
+apt install htop -y || echo "Installing htop has failed. Please check the logs or console output above."
 apt install net-tools -y || echo "Installing net-tools has failed. Please check the logs or console output above."
 
 echo ""
