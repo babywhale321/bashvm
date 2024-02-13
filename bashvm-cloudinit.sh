55299621e04f11406b9c9133c3d8eed52463b968
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2024-02-13 16:10:35 -0500

    Update bashvm-cloudinit.sh

--- bashvm-cloudinit.sh
+++ bashvm-cloudinit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -ep "Enter the new VM name: " hostname
+read -ep "Enter the new VM name: " vmname
 
 # Copy cloudinit file
 cp bashvm-cloudinit.yaml /var/lib/libvirt/images
@@ -15,15 +15,15 @@ else
     wget https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2
 fi
 
-#Enable default network
+# Enable default network
 virsh net-start default
 virsh net-autostart default
 
-#Deply VM
-virt-install --name $hostname --memory 2048 --vcpus 2 --disk=size=20,backing_store=/var/lib/libvirt/images/debian-12-generic-amd64.qcow2 --cloud-init user-data=./bashvm-cloudinit.yaml,disable=on --network bridge=virbr0 --osinfo=debian11
+# Deploy VM
+virt-install --name $vmname --memory 2048 --vcpus 2 --disk=size=20,backing_store=/var/lib/libvirt/images/debian-12-generic-amd64.qcow2 --cloud-init user-data=./bashvm-cloudinit.yaml,disable=on --network bridge=virbr0 --osinfo=debian10
 
 # Default login information
 echo "========= Login Info ========="
-echo "Hostname: $hostname"
+echo "VM Name: $vmname"
 echo "Default username: bashvm"
 echo "Default password: bashvm"
