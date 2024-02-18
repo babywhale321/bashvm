33038e358102363df2eb934230e19407f9fe0161
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2024-02-17 19:38:10 -0500

    Add files via upload

---
+++ bashvm-monitor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo ""
+model=$(lscpu | grep "Model" | grep "name")
+cpu=$(lscpu | grep "Core(s)")
+mem=$(lsmem | grep Total | grep online | grep memory)
+
+echo "Hostname: "$HOSTNAME""
+echo ""$model" "$cpu""
+echo $mem
+echo "------------------------------------------------------------------------------------------------"
+free -h
+echo "------------------------------------------------------------------------------------------------"
+sensors | grep Core
+echo "------------------------------------------------------------------------------------------------"
+ps all
+echo ""
