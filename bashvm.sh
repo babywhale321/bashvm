fb26d0f1693eb309e83a27dd868a833bf9f9dcd4
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2024-02-24 11:18:35 -0500

    Update bashvm.sh
    - add feedback to user

--- bashvm.sh
+++ bashvm.sh
@@ -623,6 +623,7 @@ while true; do
                         read -ep "Enter the VM name: " vm_name
 
                         sed -i "/#$vm_name/,/###$vm_name/d" /etc/libvirt/hooks/qemu
+                        echo "Port forwarding rules for $vm_name has been deleted."
                         ;;
 
                     4)
