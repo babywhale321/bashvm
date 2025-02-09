bafaae3c3ec0c16ea66305ae5ef4cf7f472b9eb5
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2025-02-09 13:39:08 -0500

    Update bashvm-show-port-forwarding.sh
    - Better output formatting of tables

--- bashvm-show-port-forwarding.sh
+++ bashvm-show-port-forwarding.sh
@@ -9,8 +9,7 @@ tables=$(sqlite3 "$db_file" "SELECT name FROM sqlite_master WHERE type='table';"
 # Iterate over each table and select all data
 for table in $tables; do
     echo "table: $table"
-    # Fetch and format table data
-    sqlite3 -header -csv "$db_file" "SELECT * FROM $table;" | \
-    awk -F',' 'BEGIN {OFS="\t"} {printf "%-10s %-15s %-10s %-10s %-10s\n", $1, $2, $3, $4, $5}'
+    # Fetch and format table data with dynamic column spacing
+    sqlite3 -header -csv "$db_file" "SELECT * FROM $table;" | column -s, -t
     echo ""
 done
